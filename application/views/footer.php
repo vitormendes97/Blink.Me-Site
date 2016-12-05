c2734cee48db1b609d54f0ff9d77c3c7e6786010
Vitor Mendes <vitor.mendes@salaovip.com.br>
2016-12-05 16:54:44 -0200

    .

--- application/views/footer.php
+++ application/views/footer.php
@@ -10,8 +10,10 @@
                 <div class="col-md-6 col-sm-6 col-xs-12" style="    padding: 30px 20px;">
                   <p>Av.Giovanni Gronchi,5930 - Vila Andrade, São Paulo - SP | Morumbi Town</p>
                   <p>1º Piso | 11 2503.9527</p>
+                  <p>Dias de semana das 10h as 22h. Domingos e feriados das 14h as 20h.</p>
+                    <br>
                   <p>EM BREVE</p>
-                  <br>
+
                   <div class="col-md-12 col-xs-12" style="padding-left:0;">
                     <a href="#"> <img style="height: 30px;"
                       src="public/img/google_store.jpg" id="playstore"></a>
